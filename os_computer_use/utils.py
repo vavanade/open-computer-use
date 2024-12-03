efd0f56eb47ec678b2b209a0bbec0b7b30774dc1
James Murdza <james@jamesmurdza.com>
2024-12-03 14:04:32 -0800

    Improve logging

--- os_computer_use/utils.py
+++ os_computer_use/utils.py
@@ -3,6 +3,8 @@ import re
 from os_computer_use.llm import osatlas_config
 from gradio_client import Client
 
+osatlas = Client(osatlas_config["source"])
+
 
 def draw_big_dot(image, coordinates, color="red", radius=12):
     draw = ImageDraw.Draw(image)
@@ -14,15 +16,14 @@ def draw_big_dot(image, coordinates, color="red", radius=12):
 
 def send_bbox_request(image_data, prompt):
     try:
-        client = Client(osatlas_config["source"])
-        result = client.predict(
+        result = osatlas.predict(
             image=image_data,
             text_input=prompt + "\nReturn the response in the form of a bbox",
             model_id=osatlas_config["model"],
             api_name=osatlas_config["api_name"],
         )
         midpoint = extract_bbox_midpoint(result[1])
-        print(result[2])
+        print("BBOX: " + result[2])
         if not midpoint:
             raise ValueError("The bbox response is malformed.")
         return midpoint
